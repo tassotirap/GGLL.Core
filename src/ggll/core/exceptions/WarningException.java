27da68b28863fc6d4ac2c4a9a014943fda796de6
Tasso Tirapani Silva Pinto <tasso.t@gmail.com>
2014-04-24 22:39:07 -0300

    Exception

---
+++ src/ggll/core/exceptions/WarningException.java
@@ -0,0 +1,11 @@
+package ggll.core.exceptions;
+
+public class WarningException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public WarningException(final String mesage)
+	{
+		super(mesage);
+	}
+}
